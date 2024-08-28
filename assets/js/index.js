2456b981a05c94ac7d2b4726cc7b3f5f0128270f
JesusVeraPompa <merkapompa@gmail.com>
2024-08-28 09:33:17 -0500

    4.2 Organizado el details.js y sus datos

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,38 +3,37 @@
 /*------------------------------------------------------------------------------------------------------------*/
 
 //  Importamos la vista del DOM
-import { CargarTarjetas } from './module/viewDom.js'
+import { CargarTarjetas, viewDomIndex, viewDomDetailsVacio } from './module/viewDom.js'
 
 //  Declaracion de Variables
 let contenedor = document.getElementById('contenedor')
+// Limpiar tarjetas
+function LimpiarTarjetas() {
+    while (contenedor.firstChild) {
+        contenedor.removeChild(contenedor.firstChild)
+    }
+}
 
-//  Url con la Información
-const url = 'https://mindhub-xj03.onrender.com/api/amazing'
-fetch(url)
+//  API con la Información
+const api = 'https://mindhub-xj03.onrender.com/api/amazing'
+fetch(api)
     .then((response) => response.json())
     .then((data) => {
         console.log(data)
 
         //  Tarjetas cargadar por DOM
+        LimpiarTarjetas()
         CargarTarjetas(contenedor, data)
-
-        // Limpiar tarjetas
-        function LimpiarTarjetas() {
-            while (contenedor.firstChild) {
-                contenedor.removeChild(contenedor.firstChild)
-            }
-        }
 
         //  Categorias cargadar por DOM
         let filtroCategory = data.events.map((events) => events.category)
-        //console.log(filtroCategory);
         const resultfiltroCategory = filtroCategory.reduce((acc, item) => {
             if (!acc.includes(item)) {
                 acc.push(item)
             }
             return acc
         }, [])
-        //.log(resultfiltroCategory);
+        console.log(resultfiltroCategory)
         let category = document.getElementById('category')
         for (let i = 0; i < resultfiltroCategory.length; i++) {
             //console.log(resultfiltroCategory[i]);
@@ -49,184 +48,127 @@ fetch(url)
             category.appendChild(check)
         }
 
-        //  Filtro por categoría
-        document.getElementById('category').addEventListener('change', (e) => {
-            //console.log(e.target);
-            let checkboxChekeados = document.querySelectorAll('input[type=checkbox]:checked')
-            let arreglo = Array.from(checkboxChekeados).map((e) => e.value)
-            //console.log(arreglo);
-            let nuevoArreglo = data.events.filter((e) => arreglo.includes(e.category))
-            //console.log(nuevoArreglo);
-
-            if (nuevoArreglo.length === 0) {
-                LimpiarTarjetas()
-                CargarTarjetas()
-            } else {
-                // Limpiar tarjetas
-                LimpiarTarjetas()
-                // Cargar tarjetas filtradas
-                for (let i = 0; i < nuevoArreglo.length; i++) {
-                    let tarjeta = document.createElement('div')
-                    tarjeta.className = 'tarjeta'
-                    tarjeta.innerHTML = `   
-                                <div class="card row" >
-                                <img src="${nuevoArreglo[i].image}" class="card-img-top p-2" alt="${nuevoArreglo[i].name}"/>
-                                <div class="card-body justify-content-center align-items-center">
-                                    <h5 class="card-title">${nuevoArreglo[i].name}</h5>
-                                    <p class="card-text">${nuevoArreglo[i].description}</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="col-6 d-flex justify-content-center align-items-center">
-                                            <p class="precio">Price:</p>
-                                            <h5 class="precio px-2">$ ${nuevoArreglo[i].price}</h5>
-                                        </div>
-                                        <div class="col-6">
-                                            <a id="boton${nuevoArreglo[i]._id}" href="#" class="btn btn-primary" onClick="valorDelID('${nuevoArreglo[i]._id}')">Details</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`
-                    contenedor.appendChild(tarjeta)
-                }
-
-                document.getElementById('search').addEventListener('input', (e) => {
-                    let input = e.target.value.toLowerCase()
-                    let arregloFiltroLetra = nuevoArreglo.filter(
-                        (e) =>
-                            e.name.toLowerCase().includes(input) ||
-                            e.description.toLowerCase().includes(input)
-                    )
-                    //console.log(input);
+        /*---------------------------------------- Filtro por Categoría ----------------------------------------*/
+        //  escuchamos el cambio del checkbox y lo guardamos en un array
+        document.getElementById("category").addEventListener("change", (e) => {
+            let checkboxChekeados = document.querySelectorAll(
+              "input[type=checkbox]:checked"
+            );
+            let arreglo = Array.from(checkboxChekeados).map((e) => e.value);
+            console.log(arreglo);
+  
+            //  si hay checkbox Activados
+            if (arreglo.length > 0) {
+              // creamos un array en base a los checkbox activamos
+              let nuevoArreglo = data.events.filter((e) =>
+                arreglo.includes(e.category.toString())
+              );
+              console.log(nuevoArreglo);
+  
+              if (nuevoArreglo.length === 0) {
+                LimpiarTarjetas();
+                viewDomDetailsVacio(contenedor);
+              } else {
+                //  Cargamos por DOM los datos checkbox activamos
+                LimpiarTarjetas();
+                viewDomIndex(contenedor, nuevoArreglo);
+  
+                //  Escuchamos el input y lo guardamos en un array
+                document
+                  .getElementById("search")
+                  .addEventListener("input", (e) => {
+                    let input = e.target.value.toLowerCase();
+                    const arregloFiltroLetra = nuevoArreglo.filter((e) =>
+                      e.name.toLowerCase().includes(input)
+                    );
+                    console.log(arregloFiltroLetra);
                     if (arregloFiltroLetra.length === 0) {
-                        LimpiarTarjetas()
-                        /*CargarTarjetas();*/
-                        let tarjeta = document.createElement('div')
-                        tarjeta.innerHTML = `   
-                                    <div >
-                                    <h4 class="p-2">Sorry, there is no record to display.</h4>
-                                </div>`
-                        contenedor.appendChild(tarjeta)
+                      LimpiarTarjetas();
+                      viewDomDetailsVacio(contenedor);
                     } else {
-                        // Limpiar tarjetas
-                        LimpiarTarjetas()
-                        // Cargar tarjetas filtradas
-                        for (let i = 0; i < arregloFiltroLetra.length; i++) {
-                            let tarjeta = document.createElement('div')
-                            tarjeta.className = 'tarjeta'
-                            tarjeta.innerHTML = `   
-                                    <div class="card row" >
-                                    <img src="${arregloFiltroLetra[i].image}" class="card-img-top p-2" alt="${arregloFiltroLetra[i].name}"/>
-                                    <div class="card-body justify-content-center align-items-center">
-                                    <h5 class="card-title">${arregloFiltroLetra[i].name}</h5>
-                                    <p class="card-text">${arregloFiltroLetra[i].description}</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                    <div class="col-6 d-flex justify-content-center align-items-center">
-                                    <p class="precio">Price:</p>
-                                    <h5 class="precio px-2">$ ${arregloFiltroLetra[i].price}</h5>
-                                    </div>
-                                    <div class="col-6">
-                                            <a id="boton${arregloFiltroLetra[i]._id}" href="#" class="btn btn-primary" onClick="valorDelID('${arregloFiltroLetra[i]._id}')">Details</a>
-                                    </div>
-    
-                                    </div>
-                                    </div>
-                                </div>`
-                            contenedor.appendChild(tarjeta)
-                        }
+                      //  Cargamos por DOM los datos checkbox activamos segun el filtro del input
+                      LimpiarTarjetas();
+                      viewDomIndex(contenedor, arregloFiltroLetra);
                     }
-                })
-            }
-        })
-
-        document.getElementById('search').addEventListener('input', (e) => {
-            let input = e.target.value.toLowerCase()
-            let arregloFiltroLetra = data.events.filter(
-                (e) =>
-                    e.name.toLowerCase().includes(input) ||
-                    e.description.toLowerCase().includes(input)
-            )
-            console.log(input)
-
-            if (arregloFiltroLetra.length === 0) {
-                LimpiarTarjetas()
-                /*CargarTarjetas();*/
-                let tarjeta = document.createElement('div')
-                tarjeta.innerHTML = `   
-                                <div >
-                                <h4 class="p-2">Sorry, there is no record to display.</h4>
-                            </div>`
-                contenedor.appendChild(tarjeta)
+                  });
+              }
             } else {
-                // Limpiar tarjetas
-                LimpiarTarjetas()
-                // Cargar tarjetas filtradas
-                for (let i = 0; i < arregloFiltroLetra.length; i++) {
-                    let tarjeta = document.createElement('div')
-                    tarjeta.className = 'tarjeta'
-                    tarjeta.innerHTML = `   
-                                <div class="card row" >
-                                <img src="${arregloFiltroLetra[i].image}" class="card-img-top p-2" alt="${arregloFiltroLetra[i].name}"/>
-                                <div class="card-body justify-content-center align-items-center">
-                                <h5 class="card-title">${arregloFiltroLetra[i].name}</h5>
-                                <p class="card-text">${arregloFiltroLetra[i].description}</p>
-                                <div class="d-flex justify-content-center align-items-center">
-                                <div class="col-6 d-flex justify-content-center align-items-center">
-                                <p class="precio">Price:</p>
-                                <h5 class="precio px-2">$ ${arregloFiltroLetra[i].price}</h5>
-                                </div>
-                                <div class="col-6">
-                                            <a id="boton${arregloFiltroLetra[i]._id}" href="#" class="btn btn-primary" onClick="valorDelID('${arregloFiltroLetra[i]._id}')">Details</a>
-                                </div>
-
-                                </div>
-                                </div>
-                            </div>`
-                    contenedor.appendChild(tarjeta)
+              //  si NO hay checkbox Activados
+  
+              //  Cargamos por DOM los datos del Array Final
+              LimpiarTarjetas();
+              viewDomIndex(contenedor, data.events);
+  
+              //  Escuchamos el input y lo guardamos en un array
+              document.getElementById("search").addEventListener("input", (e) => {
+                let input = e.target.value.toLowerCase();
+                const arregloFiltroLetra = data.events.filter((e) =>
+                  e.name.toLowerCase().includes(input)
+                );
+                console.log(arregloFiltroLetra);
+  
+                if (arregloFiltroLetra.length === 0) {
+                  LimpiarTarjetas();
+                  viewDomDetailsVacio(contenedor);
+                } else {
+                  //  Cargamos por DOM los datos del Array Final segun el filtro del input
+                  LimpiarTarjetas();
+                  viewDomIndex(contenedor, arregloFiltroLetra);
                 }
-
-                //  Filtro por categoría
-                document.getElementById('category').addEventListener('change', (e) => {
-                    //console.log(e.target);
-                    let checkboxChekeados = document.querySelectorAll(
-                        'input[type=checkbox]:checked'
-                    )
-                    let arreglo = Array.from(checkboxChekeados).map((e) => e.value)
-                    //console.log(arreglo);
-                    let nuevoArreglo = arregloFiltroLetra.filter((e) =>
-                        arreglo.includes(e.category)
-                    )
-                    //console.log(nuevoArreglo);
-
-                    if (nuevoArreglo.length === 0) {
-                        LimpiarTarjetas()
-                        CargarTarjetas()
-                    } else {
-                        // Limpiar tarjetas
-                        LimpiarTarjetas()
-                        // Cargar tarjetas filtradas
-                        for (let i = 0; i < arregloFiltroLetra.length; i++) {
-                            let tarjeta = document.createElement('div')
-                            tarjeta.className = 'tarjeta'
-                            tarjeta.innerHTML = `   
-                                <div class="card row" >
-                                <img src="${arregloFiltroLetra[i].image}" class="card-img-top p-2" alt="${arregloFiltroLetra[i].name}"/>
-                                <div class="card-body justify-content-center align-items-center">
-                                    <h5 class="card-title">${arregloFiltroLetra[i].name}</h5>
-                                    <p class="card-text">${arregloFiltroLetra[i].description}</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="col-6 d-flex justify-content-center align-items-center">
-                                            <p class="precio">Price:</p>
-                                            <h5 class="precio px-2">$ ${arregloFiltroLetra[i].price}</h5>
-                                        </div>
-                                        <div class="col-6">
-                                            <a id="boton${arregloFiltroLetra[i]._id}" href="#" class="btn btn-primary" onClick="valorDelID('${arregloFiltroLetra[i]._id}')">Details</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>`
-                            contenedor.appendChild(tarjeta)
-                        }
-                    }
-                })
+              });
             }
-        })
+          });
+  
+          /*---------------------------------------- Filtro por Buscar ----------------------------------------*/
+          //  Escuchamos el input y lo guardamos en un array
+          document.getElementById("search").addEventListener("input", (e) => {
+            let input = e.target.value.toLowerCase();
+            const arregloFiltroLetra = data.events.filter((e) =>
+              e.name.toLowerCase().includes(input)
+            );
+            console.log(arregloFiltroLetra);
+  
+            if (arregloFiltroLetra.length === 0) {
+              LimpiarTarjetas();
+              viewDomDetailsVacio(contenedor);
+            } else {
+              //  Cargamos por DOM los datos que guardamos en un array
+              LimpiarTarjetas();
+              viewDomIndex(contenedor, arregloFiltroLetra);
+  
+              //  escuchamos el cambio del checkbox y lo guardamos en un array
+              document
+                .getElementById("category")
+                .addEventListener("change", (e) => {
+                  let checkboxChekeados = document.querySelectorAll(
+                    "input[type=checkbox]:checked"
+                  );
+                  let arreglo = Array.from(checkboxChekeados).map((e) => e.value);
+                  console.log(arreglo);
+  
+                  //  si hay checkbox Activados
+                  if (arreglo.length > 0) {
+                    // creamos un array en base a los checkbox activamos
+                    let nuevoArreglo = arregloFiltroLetra.filter((e) =>
+                      arreglo.includes(e.category)
+                    );
+                    console.log(nuevoArreglo);
+  
+                    if (nuevoArreglo.length === 0) {
+                      LimpiarTarjetas();
+                      viewDomDetailsVacio(contenedor);
+                    } else {
+                      //  Cargamos por DOM los datos del filtro del input segun el checkbox activo
+                      LimpiarTarjetas();
+                      viewDomIndex(contenedor, nuevoArreglo);
+                    }
+                  } else {
+                    //  si NO hay checkbox Activados
+                    //  Cargamos por DOM los datos del filtro del input segun lo guardamos en un array inical del input
+                    LimpiarTarjetas();
+                    viewDomIndex(contenedor, arregloFiltroLetra);
+                  }
+                });
+            }
+          });
     })
--- assets/js/module/viewDom.js
+++ assets/js/module/viewDom.js
@@ -22,3 +22,37 @@ export let CargarTarjetas = (idHtmlConteiner, data) => {
         idHtmlConteiner.appendChild(tarjeta)
     }
 }
+
+export let viewDomIndex = (idHtmlConteiner, data) => {
+    for (let i = 0; i < data.length; i++) {
+        let tarjeta = document.createElement('div')
+        tarjeta.className = 'tarjeta'
+        tarjeta.innerHTML = `   <div class="card row" >
+                                    <img src="${data[i].image}" class="card-img-top p-2" alt="${data[i].name}"/>
+                                    <div class="card-body justify-content-center align-items-center">
+                                        <h5 class="card-title">${data[i].name}</h5>
+                                        <p class="card-text">${data[i].description}</p>
+                                        <div class="d-flex justify-content-center align-items-center">
+                                            <div class="col-6 d-flex justify-content-center align-items-center">
+                                                <p class="precio">Price:</p>
+                                                <h5 class="precio px-2">$ ${data[i].price}</h5>
+                                            </div>
+                                            <div class="col-6">
+                                                <a id="boton${data[i]._id}" href="./pages/details.html?id=${data[i]._id}" class="btn btn-primary" onClick="valorDelID('${data[i]._id}')">Details</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`
+
+        idHtmlConteiner.appendChild(tarjeta)
+    }
+}
+
+export let viewDomDetailsVacio = (idHtmlConteiner) => {
+    let tarjeta = document.createElement("div");
+    tarjeta.innerHTML = `   
+                                <div >
+                                    <h4 class="p-2">Sorry, there are no records to display.</h4>
+                                </div>`;
+    idHtmlConteiner.appendChild(tarjeta);
+};
